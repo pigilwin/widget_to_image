5d7e33eb3ebe3ac1e9562cc81bd8207b77f60e9f
Tim Remnant <timwebdesign2013@gmail.com>
2021-07-10 20:57:55 +0100

    Adding tests

--- test/widget_to_image_test.dart
+++ test/widget_to_image_test.dart
@@ -1,12 +1,29 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:widget_to_image/encoded_widget.dart';
 
 import 'package:widget_to_image/widget_to_image.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('Create a text widget with just letters and convert to base64', (WidgetTester tester) async {
+
+    var base = '';
+
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: WidgetToImage(
+          child: Text('base64'),
+          pixelRatio: 3.0,
+          format: ImageByteFormat.png,
+          callback: (EncodedWidget widget) {
+            base = widget.toBase64();
+          },
+        ),
+      ),
+    ));
+
+    print(base);
   });
 }
